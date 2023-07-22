d1da7a0717e9a94861611765b63548fc5bbf6d06
Boris Antonijev <antonijevboris@gmail.com>
2023-07-22 22:17:09 +0200

    initial project

---
+++ src/main/java/com/bnm/valorantstrategyplanner/ValorantStrategyPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.bnm.valorantstrategyplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorantStrategyPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValorantStrategyPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnm/valorantstrategyplanner/ValorantStrategyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnm.valorantstrategyplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValorantStrategyPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
