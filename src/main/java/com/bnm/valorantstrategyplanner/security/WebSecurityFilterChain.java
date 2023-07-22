bc2596adec362d6da56c6612b8031ba34cb77c51
Boris Antonijev <antonijevboris@gmail.com>
2023-07-22 22:39:47 +0200

    security filter chain initialize

---
+++ src/main/java/com/bnm/valorantstrategyplanner/security/WebSecurityFilterChain.java
@@ -0,0 +1,35 @@
+package com.bnm.valorantstrategyplanner.security;
+
+import lombok.Data;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@Data
+@EnableMethodSecurity(securedEnabled = true) // enable authorization (roles for url)
+public class WebSecurityFilterChain {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request ->
+                        request
+                                .requestMatchers("/api/v1/auth/**")
+                                .permitAll().anyRequest().authenticated()
+                )
+                .sessionManagement(manager ->
+                    manager
+                            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        return http.build();
+    }
+}
