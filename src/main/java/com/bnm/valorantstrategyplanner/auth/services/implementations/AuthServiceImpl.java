d20312cef2b2e7d3ff1a88797e58714b8fb969b9
Boris Antonijev <antonijevboris@gmail.com>
2023-07-27 12:02:16 +0200

    Update AuthServiceImpl.java

--- src/main/java/com/bnm/valorantstrategyplanner/auth/services/implementations/AuthServiceImpl.java
+++ src/main/java/com/bnm/valorantstrategyplanner/auth/services/implementations/AuthServiceImpl.java
@@ -12,7 +12,6 @@ import com.bnm.valorantstrategyplanner.utils.PasswordManagerImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
