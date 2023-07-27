a2d6fd416f7ea30d27d202207da688e468c62d3f
Boris Antonijev <antonijevboris@gmail.com>
2023-07-27 13:17:01 +0200

    Update ValorantStrategyPlannerApplication.java

--- src/main/java/com/bnm/valorantstrategyplanner/ValorantStrategyPlannerApplication.java
+++ src/main/java/com/bnm/valorantstrategyplanner/ValorantStrategyPlannerApplication.java
@@ -2,8 +2,6 @@ package com.bnm.valorantstrategyplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication()
 public class ValorantStrategyPlannerApplication {
