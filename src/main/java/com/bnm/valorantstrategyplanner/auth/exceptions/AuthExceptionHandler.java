def9f436f328b129fb87b4626a02491f1f1bae2b
Boris Antonijev <antonijevboris@gmail.com>
2023-07-27 13:12:00 +0200

    no securuty

--- src/main/java/com/bnm/valorantstrategyplanner/auth/exceptions/AuthExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package com.bnm.valorantstrategyplanner.auth.exceptions;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice
-public class AuthExceptionHandler {
-
-}
--- src/main/java/com/bnm/valorantstrategyplanner/users/exceptions/throwables/UserExistException.java
+++
@@ -1,7 +0,0 @@
-package com.bnm.valorantstrategyplanner.users.exceptions.throwables;
-
-public class UserExistException extends RuntimeException {
-    public UserExistException() {
-        super("User exist.");
-    }
-}
