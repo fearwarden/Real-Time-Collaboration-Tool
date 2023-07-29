054dad55794019a8975e8d297deda68aed49fb42
Boris Antonijev <antonijevboris@gmail.com>
2023-07-29 22:44:04 +0200

    jett agent

---
+++ frontend/src/app/models/agents/literals/Jett.ts
@@ -0,0 +1,8 @@
+import AbstractAgent from "../AbstractAgent";
+import JettImage from "../../../../assets/img/agents/jett/Jett_icon.webp";
+
+export default class Jett extends AbstractAgent {
+  constructor() {
+    super("Jett", JettImage);
+  }
+}
