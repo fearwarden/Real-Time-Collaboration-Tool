a51069a946d1b1c22eee92bd64f3caeb0ed9b71d
Boris Antonijev <antonijevboris@gmail.com>
2023-07-29 23:31:18 +0200

    viper

---
+++ frontend/src/app/models/agents/literals/Viper.ts
@@ -0,0 +1,8 @@
+import AbstractAgent from "../AbstractAgent";
+import ViperImage from "../../../../assets/img/agents/viper/Viper_icon.webp";
+
+export default class Viper extends AbstractAgent {
+  constructor() {
+    super("Viper", ViperImage);
+  }
+}
