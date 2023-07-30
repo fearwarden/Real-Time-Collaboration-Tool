3c93aba4206ab33ce207191ee3bffec937080ca9
Boris Antonijev <antonijevboris@gmail.com>
2023-07-30 13:44:32 +0200

    omen

---
+++ frontend/src/app/models/spells/literals/omen/ShroudedStep.ts
@@ -0,0 +1,8 @@
+import AbstractSpell from "../../AbstractSpell";
+import ShroudedStepImage from "../../../../../assets/img/agents/omen/Shrouded_Step.webp";
+
+export default class ShroudedStep extends AbstractSpell {
+  constructor() {
+    super("Omen", "Shrouded Step", ShroudedStepImage, "basic");
+  }
+}
