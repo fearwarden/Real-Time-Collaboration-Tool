7f6c73e1aad65b03a39a933b6bc219c51fa8084c
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2023-08-01 13:09:02 +0200

    moved agents and spell to content folder

---
+++ frontend/src/app/models/AgentNode.ts
@@ -0,0 +1,21 @@
+import AbstractAgent from "../content/agents/AbstractAgent";
+import MovableElementNode from "./elements/MovableElementNode";
+
+export default class AgentNode extends MovableElementNode {
+  private _agentContent: AbstractAgent;
+
+  constructor(
+    agent: AbstractAgent,
+    x: number,
+    y: number,
+    width: number,
+    height: number
+  ) {
+    super(x, y, height, width);
+    this._agentContent = agent;
+  }
+
+  public get agentContent(): AbstractAgent {
+    return this._agentContent;
+  }
+}
--- frontend/src/app/view/Canvas.ts
+++ frontend/src/app/view/Canvas.ts
@@ -1,77 +1,100 @@
 import Main from "../Main";
+import ElementNode from "../models/elements/ElementNode";
+import ISubscriber from "../utils/observer/ISubscriber";
 
-export default class Canvas {
+export default class Canvas implements ISubscriber {
+  private _canvasElement: HTMLCanvasElement;
 
-    private _canvasElement: HTMLCanvasElement;
-    
-    private _width: number;
-    
-    private _height: number;
-    
+  private _width: number;
 
-    //TODO: Add functionality to redraw the canvas when window changes size
-    constructor(canvas: HTMLCanvasElement) {
-        this._canvasElement = canvas;
-        
-        this._width = this.canvasElement.clientWidth;
-        this._height = this.canvasElement.clientHeight;
+  private _height: number;
+  private _ctx: CanvasRenderingContext2D;
 
-        this.fixCanvasScalling();
+  //TODO: Add functionality to redraw the canvas when window changes size
+  constructor(canvas: HTMLCanvasElement) {
+    this._canvasElement = canvas;
 
-        this.setupMousePositionTracker();
+    this._width = this.canvasElement.clientWidth;
+    this._height = this.canvasElement.clientHeight;
+    this._ctx = canvas.getContext("2d")!;
+
+    this.fixCanvasScalling();
+
+    this.setupMousePositionTracker();
+  }
+  update(notification: any): void {
+    if (notification instanceof ElementNode) {
+      this.redrawCanvas();
     }
+  }
 
-    public drawDot(x: number, y: number) { // x and y are procentages of the cursor position to account for different screen sizes
-        const dotSize = 10;
+  public drawDot(x: number, y: number) {
+    // x and y are procentages of the cursor position to account for different screen sizes
+    const dotSize = 10;
 
-        //Validate it is inside boundaries
-        x = Math.max(dotSize, Math.min(this.width - dotSize, x * this.width));
-        y = Math.max(dotSize, Math.min(this.height - dotSize, y * this.height));
+    //Validate it is inside boundaries
+    x = Math.max(dotSize, Math.min(this.width - dotSize, x * this.width));
+    y = Math.max(dotSize, Math.min(this.height - dotSize, y * this.height));
 
-        const ctx = this.canvasElement.getContext('2d')!;
-        ctx.clearRect(0, 0, this.width, this.height);
-        ctx.beginPath();
-        ctx.arc(x, y, dotSize, 0, 2 * Math.PI);
-        ctx.fillStyle = 'blue';
-        ctx.fill();
-        ctx.closePath();
-    }
+    this.clearCanvas();
+    this._ctx.beginPath();
+    this._ctx.arc(x, y, dotSize, 0, 2 * Math.PI);
+    this._ctx.fillStyle = "blue";
+    this._ctx.fill();
+    this._ctx.closePath();
+  }
 
-    private setupMousePositionTracker() {
-        this.canvasElement.addEventListener("mousemove", (e) => {
-            if (e.target instanceof HTMLCanvasElement) {
-                Main.getInstance().stompClient.send("/live/mouse-position", {
-                    userId: Main.getInstance().userId,
-                    x: (e.clientX - e.target!.offsetLeft) / this.width,
-                    y: (e.clientY - e.target!.offsetTop) / this.height
-                })
-            }
-        })
-    }
+  private setupMousePositionTracker() {
+    this.canvasElement.addEventListener("mousemove", (e) => {
+      if (e.target instanceof HTMLCanvasElement) {
+        Main.getInstance().stompClient.send("/live/mouse-position", {
+          userId: Main.getInstance().userId,
+          x: (e.clientX - e.target!.offsetLeft) / this.width,
+          y: (e.clientY - e.target!.offsetTop) / this.height,
+        });
+      }
+    });
+  }
 
-    private fixCanvasScalling() {
-        this.width = this.canvasElement.clientWidth;
-        this.height = this.canvasElement.clientHeight;
-        this.canvasElement.width = this.width; // Fix canvas scalling - https://stackoverflow.com/questions/1664785/resize-html5-canvas-to-fit-window
-        this.canvasElement.height = this.height;
-    }
+  private fixCanvasScalling() {
+    this.width = this.canvasElement.clientWidth;
+    this.height = this.canvasElement.clientHeight;
+    this.canvasElement.width = this.width; // Fix canvas scalling - https://stackoverflow.com/questions/1664785/resize-html5-canvas-to-fit-window
+    this.canvasElement.height = this.height;
+  }
 
-    public get canvasElement(): HTMLCanvasElement {
-        return this._canvasElement;
-    }
-    public set canvasElement(value: HTMLCanvasElement) {
-        this._canvasElement = value;
-    }
-    public get width(): number {
-        return this._width;
-    }
-    public set width(value: number) {
-        this._width = value;
-    }
-    public get height(): number {
-        return this._height;
-    }
-    public set height(value: number) {
-        this._height = value;
-    }
+  private redrawCanvas() {
+    // Clear whole canvas in order to redraw the whole context
+    this.clearCanvas();
+  }
+
+  private drawCanvas(): void {
+    const data = Main.getInstance().strategyModel.elements;
+    data.forEach((element: ElementNode) => {
+      console.log(element);
+    });
+  }
+
+  private clearCanvas(): void {
+    this._ctx?.clearRect(0, 0, this.width, this.height);
+  }
+
+  public get canvasElement(): HTMLCanvasElement {
+    return this._canvasElement;
+  }
+  public set canvasElement(value: HTMLCanvasElement) {
+    this._canvasElement = value;
+  }
+  public get width(): number {
+    return this._width;
+  }
+  public set width(value: number) {
+    this._width = value;
+  }
+  public get height(): number {
+    return this._height;
+  }
+  public set height(value: number) {
+    this._height = value;
+  }
 }
