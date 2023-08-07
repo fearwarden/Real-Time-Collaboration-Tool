223d4016f9269861e7e3bc791f797bff34ddc633
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2023-08-07 12:44:38 +0200

    Update Canvas.ts

--- frontend/src/app/view/Canvas.ts
+++ frontend/src/app/view/Canvas.ts
@@ -6,213 +6,305 @@ import ISubscriber from "../utils/observer/ISubscriber";
 import ElementView from "./ElementView";
 import MapView from "./MapView";
 
-
 export default class Canvas implements ISubscriber {
-	private _canvasElement: HTMLCanvasElement;
+  private _canvasElement: HTMLCanvasElement;
 
-	private _width: number;
+  private _width: number;
+  private _height: number;
+  private _ctx: CanvasRenderingContext2D;
 
-	private _height: number;
-	private _ctx: CanvasRenderingContext2D;
+  // Camera zoom variables
+  private cameraOffset: { x: number; y: number };
+  private cameraZoom = 1;
+  private MAX_ZOOM = 2;
+  private MIN_ZOOM = 1;
+  private SCROLL_SENSITIVITY = 0.005;
+  private initialPinchDistance = null;
+  private lastZoom = this.cameraZoom;
+  private isDragging = false;
+  private dragStart = { x: 0, y: 0 };
 
-	private _elementList: ElementView[];
-	private _activeElement: ElementView | null;
+  private _elementList: ElementView[];
+  private _activeElement: ElementView | null;
 
-	private _mapView: MapView;
+  private _mapView: MapView;
 
-	//TODO: Add functionality to redraw the canvas when window changes size
-	constructor(canvas: HTMLCanvasElement) {
-		this._canvasElement = canvas;
+  //TODO: Add functionality to redraw the canvas when window changes size
+  constructor(canvas: HTMLCanvasElement) {
+    this._canvasElement = canvas;
 
-		this._width = this.canvasElement.clientWidth;
-		this._height = this.canvasElement.clientHeight;
-		this._ctx = canvas.getContext("2d")!;
+    this._width = this.canvasElement.clientWidth;
+    this._height = this.canvasElement.clientHeight;
+    this._ctx = canvas.getContext("2d")!;
 
-		this._elementList = [];
-		this._activeElement = null;
+    this.cameraOffset = { x: 0, y: 0 };
 
-		this._mapView = new MapView();
+    this._elementList = [];
+    this._activeElement = null;
 
-		this.fixCanvasScalling();
+    this._mapView = new MapView();
 
-		this.setupMousePositionTracker();
-		this.setupHoveredElementTracker();
-		this.setupElementDragging();
-	}
-	update(notification: any): void {
-	}
+    this.fixCanvasScalling();
 
-	public drawDot(x: number, y: number) {
-		// x and y are procentages of the cursor position to account for different screen sizes
-		const dotSize = 10;
+    this.setupMousePositionTracker();
+    this.setupHoveredElementTracker();
+    this.setupElementDragging();
+    this.setupCameraZoom();
+  }
+  update(notification: any): void {}
 
-		//Validate it is inside boundaries
-		x = Math.max(dotSize, Math.min(this.width - dotSize, x * this.width));
-		y = Math.max(dotSize, Math.min(this.height - dotSize, y * this.height));
+  public drawDot(x: number, y: number) {
+    // x and y are procentages of the cursor position to account for different screen sizes
+    const dotSize = 10;
 
-		this.clearCanvas();
-		this._ctx.beginPath();
-		this._ctx.arc(x, y, dotSize, 0, 2 * Math.PI);
-		this._ctx.fillStyle = "blue";
-		this._ctx.fill();
-		this._ctx.closePath();
-	}
+    //Validate it is inside boundaries
+    x = Math.max(dotSize, Math.min(this.width - dotSize, x * this.width));
+    y = Math.max(dotSize, Math.min(this.height - dotSize, y * this.height));
 
-	private setupMousePositionTracker() {
-		this.canvasElement.addEventListener("mousemove", (e) => {
-			if (e.target instanceof HTMLCanvasElement) {
-				Main.getInstance().stompClient.send("/live/mouse-position", {
-					userId: Main.getInstance().userId,
-					x: (e.clientX - e.target!.offsetLeft) / this.width,
-					y: (e.clientY - e.target!.offsetTop) / this.height,
-				});
-			}
-		});
-	}
+    this.clearCanvas();
+    this._ctx.beginPath();
+    this._ctx.arc(x, y, dotSize, 0, 2 * Math.PI);
+    this._ctx.fillStyle = "blue";
+    this._ctx.fill();
+    this._ctx.closePath();
+  }
 
-	private setupHoveredElementTracker() {
-		let isMouseDown = false;
-		this.canvasElement.addEventListener("mousemove", (e) => {
-			if (isMouseDown) return;
-			if (e.target instanceof HTMLCanvasElement) {
-				const x: number = e.clientX - e.target.offsetLeft;
-				const y: number = e.clientY - e.target.offsetTop;
+  private setupMousePositionTracker() {
+    this.canvasElement.addEventListener("mousemove", (e) => {
+      if (e.target instanceof HTMLCanvasElement) {
+        Main.getInstance().stompClient.send("/live/mouse-position", {
+          userId: Main.getInstance().userId,
+          x: (e.clientX - e.target!.offsetLeft) / this.width,
+          y: (e.clientY - e.target!.offsetTop) / this.height,
+        });
+      }
+    });
+  }
 
-				for (let element of this.elementList) {
-					const elementGeometry = element.getGeometry();
-					if (
-						CoordinateUtils.checkCoordinatesInElement({ x, y }, elementGeometry)
-					) {
-						this.activeElement = element;
-						this.enableCursor();
-						return;
-					}
-				}
-				this.activeElement = null;
-				this.disableCursor();
-			}
-		});
-		this.canvasElement.addEventListener("mousedown", () => {
-			isMouseDown = true;
-		})
-		this.canvasElement.addEventListener("mouseup", () => {
-			isMouseDown = false;
-		})
+  private setupHoveredElementTracker() {
+    let isMouseDown = false;
+    this.canvasElement.addEventListener("mousemove", (e) => {
+      if (isMouseDown) return;
+      if (e.target instanceof HTMLCanvasElement) {
+        const x: number = e.clientX - e.target.offsetLeft;
+        const y: number = e.clientY - e.target.offsetTop;
 
-	}
+        for (let element of this.elementList) {
+          const elementGeometry = element.getGeometry();
+          if (
+            CoordinateUtils.checkCoordinatesInElement({ x, y }, elementGeometry)
+          ) {
+            this.activeElement = element;
+            this.enableCursor();
+            return;
+          }
+        }
+        this.activeElement = null;
+        this.disableCursor();
+      }
+    });
+    this.canvasElement.addEventListener("mousedown", () => {
+      isMouseDown = true;
+    });
+    this.canvasElement.addEventListener("mouseup", () => {
+      isMouseDown = false;
+    });
+  }
 
-	private setupElementDragging() {
-		// When mouse is pressed
-		this.canvasElement.addEventListener("mousedown", (e) => {
-			if (this.activeElement?.elementNode instanceof MovableElementNode) {
-				if (e.target instanceof HTMLCanvasElement) {
-					const x: number = e.clientX - e.target.offsetLeft;
-					const y: number = e.clientY - e.target.offsetTop;
-					this.activeElement.elementNode.handleMousePressed(x, y);
+  private setupElementDragging() {
+    // When mouse is pressed
+    this.canvasElement.addEventListener("mousedown", (e) => {
+      if (this.activeElement?.elementNode instanceof MovableElementNode) {
+        if (e.target instanceof HTMLCanvasElement) {
+          const x: number = e.clientX - e.target.offsetLeft;
+          const y: number = e.clientY - e.target.offsetTop;
+          this.activeElement.elementNode.handleMousePressed(x, y);
 
-					const elementMoving = this.elementList.splice(this.elementList.indexOf(this.activeElement), 1)[0];
-					this.elementList.unshift(elementMoving);
-				}
-			}
-		});
+          const elementMoving = this.elementList.splice(
+            this.elementList.indexOf(this.activeElement),
+            1
+          )[0];
+          this.elementList.unshift(elementMoving);
+        }
+      }
+    });
 
-		// When mouse is pressed and moving
-		this.canvasElement.addEventListener("mousemove", (e) => {
-			if (this.activeElement?.elementNode instanceof MovableElementNode) {
-				if (e.target instanceof HTMLCanvasElement) {
-					const x: number = e.clientX - e.target.offsetLeft;
-					const y: number = e.clientY - e.target.offsetTop;
-					this.activeElement.elementNode.handleMouseMove(x, y);
-				}
-			}
-		});
+    // When mouse is pressed and moving
+    this.canvasElement.addEventListener("mousemove", (e) => {
+      if (this.activeElement?.elementNode instanceof MovableElementNode) {
+        if (e.target instanceof HTMLCanvasElement) {
+          const x: number = e.clientX - e.target.offsetLeft;
+          const y: number = e.clientY - e.target.offsetTop;
+          this.activeElement.elementNode.handleMouseMove(x, y);
+        }
+      }
+    });
 
-		// When mouse is released
-		this.canvasElement.addEventListener("mouseup", () => {
-			if (this.activeElement?.elementNode instanceof MovableElementNode) {
-				this.activeElement.elementNode.handleMouseReleased();
-			}
-		});
-	}
+    // When mouse is released
+    this.canvasElement.addEventListener("mouseup", () => {
+      if (this.activeElement?.elementNode instanceof MovableElementNode) {
+        this.activeElement.elementNode.handleMouseReleased();
+      }
+    });
+  }
 
-	private fixCanvasScalling() {
-		this.width = this.canvasElement.clientWidth;
-		this.height = this.canvasElement.clientHeight;
-		this.canvasElement.width = this.width; // Fix canvas scalling - https://stackoverflow.com/questions/1664785/resize-html5-canvas-to-fit-window
-		this.canvasElement.height = this.height;
-	}
+  private setupCameraZoom() {
+    this.canvasElement.addEventListener("mousedown", (e) => {
+      if (e.target instanceof HTMLCanvasElement) {
+        //Mouse down
+        this.isDragging = true;
+        this.dragStart.x =
+          (e.clientX - e.target.offsetLeft) / this.cameraZoom -
+          this.cameraOffset.x;
+        this.dragStart.y =
+          (e.clientY - e.target.offsetTop) / this.cameraZoom -
+          this.cameraOffset.y;
+      }
+    });
 
-	public drawImage(image: HTMLImageElement, x: number, y: number, width: number = -1, height: number = -1): void {
-		if (width > 0 && height > 0) {
-			this._ctx.drawImage(image, x, y, width, height);
-			return;
-		}
-		this._ctx.drawImage(image, x, y);
-	}
-	public redrawCanvas = () => {
-		// Clear whole canvas in order to redraw the whole context
-		this.clearCanvas();
-		this.drawCanvas();
-		requestAnimationFrame(this.redrawCanvas);
-	}
+    this.canvasElement.addEventListener("mouseup", (e) => {
+      //Mouse up
+      this.isDragging = false;
+      this.lastZoom = this.cameraZoom;
+    });
 
-	private drawCanvas(): void {
-		this.mapView.loadMap();
-		for (let i = this.elementList.length - 1; i >= 0; i--) {
-			this.elementList[i].draw();
-		}
-	}
+    this.canvasElement.addEventListener("mouseleave", (e) => {
+      //Mouse up
+      this.isDragging = false;
+      this.lastZoom = this.cameraZoom;
+    });
 
-	public start(): void {
-		requestAnimationFrame(this.redrawCanvas);
-	}
+    this.canvasElement.addEventListener("mousemove", (e) => {
+      if (e.target instanceof HTMLCanvasElement) {
+        //Mouse move
+        if (
+          this.isDragging &&
+          this.activeElement === null &&
+          this.cameraZoom !== 1
+        ) {
+          this.cameraOffset.x =
+            (e.clientX - e.target.offsetLeft) / this.cameraZoom -
+            this.dragStart.x;
+          this.cameraOffset.y =
+            (e.clientY - e.target.offsetTop) / this.cameraZoom -
+            this.dragStart.y;
+        }
+      }
+    });
 
-	private clearCanvas(): void {
-		this._ctx?.clearRect(0, 0, this.width, this.height);
-	}
+    this.canvasElement.addEventListener("wheel", (e) =>
+      this.adjustZoom(e.deltaY * this.SCROLL_SENSITIVITY)
+    );
+  }
 
-	private enableCursor() {
-		this.canvasElement.style.cursor = "pointer";
-	}
+  private adjustZoom(zoomAmount?: number, zoomFactor?: number) {
+    if (!this.isDragging) {
+      if (zoomAmount) {
+        this.cameraZoom -= zoomAmount;
+      } else if (zoomFactor) {
+        this.cameraZoom = zoomFactor * this.lastZoom;
+      }
 
-	private disableCursor() {
-		this.canvasElement.style.cursor = "auto";
-	}
+      this.cameraZoom = Math.min(this.cameraZoom, this.MAX_ZOOM);
+      this.cameraZoom = Math.max(this.cameraZoom, this.MIN_ZOOM);
+    }
+  }
 
-	public get canvasElement(): HTMLCanvasElement {
-		return this._canvasElement;
-	}
-	public set canvasElement(value: HTMLCanvasElement) {
-		this._canvasElement = value;
-	}
-	public get width(): number {
-		return this._width;
-	}
-	public set width(value: number) {
-		this._width = value;
-	}
-	public get height(): number {
-		return this._height;
-	}
-	public set height(value: number) {
-		this._height = value;
-	}
-	public get elementList(): ElementView[] {
-		return this._elementList;
-	}
-	public set elementList(value: ElementView[]) {
-		this._elementList = value;
-	}
-	private get activeElement(): ElementView | null {
-		return this._activeElement;
-	}
-	private set activeElement(value: ElementView | null) {
-		this._activeElement = value;
-	}
-	public get mapView(): MapView {
-		return this._mapView;
-	}
-	public set mapView(value: MapView) {
-		this._mapView = value;
-	}
+  private fixCanvasScalling() {
+    this.width = this.canvasElement.clientWidth;
+    this.height = this.canvasElement.clientHeight;
+    this.canvasElement.width = this.width; // Fix canvas scalling - https://stackoverflow.com/questions/1664785/resize-html5-canvas-to-fit-window
+    this.canvasElement.height = this.height;
+  }
+
+  public drawImage(
+    image: HTMLImageElement,
+    x: number,
+    y: number,
+    width: number = -1,
+    height: number = -1
+  ): void {
+    if (width > 0 && height > 0) {
+      this._ctx.drawImage(image, x, y, width, height);
+      return;
+    }
+    this._ctx.drawImage(image, x, y);
+  }
+  public redrawCanvas = () => {
+    // Clear whole canvas in order to redraw the whole context
+    this._ctx.setTransform(1, 0, 0, 1, 0, 0);
+    this.clearCanvas();
+    this.scaleAndTranslateCanvas();
+    this.drawCanvas();
+    requestAnimationFrame(this.redrawCanvas);
+  };
+
+  private drawCanvas(): void {
+    this.mapView.loadMap();
+    for (let i = this.elementList.length - 1; i >= 0; i--) {
+      this.elementList[i].draw();
+    }
+  }
+
+  private scaleAndTranslateCanvas() {
+    this._ctx.translate(this.width / 2, this.height / 2);
+    this._ctx.scale(this.cameraZoom, this.cameraZoom);
+    this._ctx.translate(
+      -this.width / 2 + this.cameraOffset.x,
+      -this.height / 2 + this.cameraOffset.y
+    );
+  }
+
+  public start(): void {
+    this.redrawCanvas();
+  }
+
+  private clearCanvas(): void {
+    this._ctx?.clearRect(0, 0, this.width, this.height);
+  }
+
+  private enableCursor() {
+    this.canvasElement.style.cursor = "pointer";
+  }
+
+  private disableCursor() {
+    this.canvasElement.style.cursor = "auto";
+  }
+
+  public get canvasElement(): HTMLCanvasElement {
+    return this._canvasElement;
+  }
+  public set canvasElement(value: HTMLCanvasElement) {
+    this._canvasElement = value;
+  }
+  public get width(): number {
+    return this._width;
+  }
+  public set width(value: number) {
+    this._width = value;
+  }
+  public get height(): number {
+    return this._height;
+  }
+  public set height(value: number) {
+    this._height = value;
+  }
+  public get elementList(): ElementView[] {
+    return this._elementList;
+  }
+  public set elementList(value: ElementView[]) {
+    this._elementList = value;
+  }
+  private get activeElement(): ElementView | null {
+    return this._activeElement;
+  }
+  private set activeElement(value: ElementView | null) {
+    this._activeElement = value;
+  }
+  public get mapView(): MapView {
+    return this._mapView;
+  }
+  public set mapView(value: MapView) {
+    this._mapView = value;
+  }
 }
