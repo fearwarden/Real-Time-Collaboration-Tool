ed0c8bb1728d5936308622f912d38c1f09c92c07
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2023-08-10 15:15:34 +0200

    Update UndoAction.ts

--- frontend/src/app/controller/actions/literals/UndoAction.ts
+++ frontend/src/app/controller/actions/literals/UndoAction.ts
@@ -5,7 +5,6 @@ export default class UndoAction implements IAction {
   doAction(event: React.MouseEvent<HTMLElement, MouseEvent>): void {
     console.log(event.target);
     if (event.target instanceof HTMLButtonElement) {
-      console.log("Sdadsa");
       Main.getInstance().commandManager.undoCommand();
     }
   }
