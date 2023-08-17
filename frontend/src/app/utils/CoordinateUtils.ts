d3c37f1a8cb5a82270493e53747005f9cb2aeb18
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2023-08-17 13:07:10 +0200

    Update CoordinateUtils.ts

--- frontend/src/app/utils/CoordinateUtils.ts
+++ frontend/src/app/utils/CoordinateUtils.ts
@@ -53,6 +53,15 @@ export default class CoordinateUtils {
     };
   }
 
+  /**
+   * Computes the minimum Euclidean distance from a point to a line segment.
+   *
+   * @param px, py - The x and y coordinates of the mouse.
+   * @param x1, y1 - The x and y coordinates of the first endpoint of the line segment.
+   * @param x2, y2 - The x and y coordinates of the second endpoint of the line segment.
+   *
+   * @returns The shortest distance from the point (px, py) to the line segment defined by (x1, y1) and (x2, y2).
+   */
   public static pointToSegmentDistance(
     px: number,
     py: number,
@@ -61,16 +70,24 @@ export default class CoordinateUtils {
     x2: number,
     y2: number
   ): number {
+    // Calculate the squared length of the line segment
     const l2 = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
-    if (l2 === 0)
-      return Math.sqrt((px - x1) * (px - x1) + (py - y1) * (py - y1)); // ako je 0 znaci da je tacka u pitanju i vracamo euclidian distance izmedju misa i tacke na liniji
 
+    // If the length is zero, the segment is actually a point
+    // In this case, return the Euclidean distance between the point (px, py) and (x1, y1)
+    if (l2 === 0)
+      return Math.sqrt((px - x1) * (px - x1) + (py - y1) * (py - y1));
+
+    // Project the point (px, py) onto the line segment, determining where it falls between (x1, y1) and (x2, y2)
+    // The variable 't' represents the relative position of the projection on the segment
     let t = ((px - x1) * (x2 - x1) + (py - y1) * (y2 - y1)) / l2;
     t = Math.max(0, Math.min(1, t));
 
+    // Compute the coordinates of the projection of (px, py) onto the segment
     const projX = x1 + t * (x2 - x1);
     const projY = y1 + t * (y2 - y1);
 
+    // Return the Euclidean distance between the point (px, py) and its projection onto the segment
     return Math.sqrt((px - projX) * (px - projX) + (py - projY) * (py - projY));
   }
 }
