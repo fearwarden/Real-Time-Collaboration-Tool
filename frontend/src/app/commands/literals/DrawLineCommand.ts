03b579d0d4daa15a9f882dd1f2af59eee4cfa217
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2023-08-18 12:27:22 +0200

    drawing line add to command pattern

---
+++ frontend/src/app/commands/literals/DrawLineCommand.ts
@@ -0,0 +1,41 @@
+import Main from "../../Main";
+import DrawingPoint from "../../models/DrawingPoint";
+import ICommand from "../ICommand";
+
+export default class DrawLineCommand implements ICommand {
+  private _drawingPoints: DrawingPoint[];
+
+  constructor(drawingPoints: DrawingPoint[]) {
+    this._drawingPoints = drawingPoints;
+  }
+
+  doCommand(): void {
+    if (
+      Main.getInstance().strategyModel.drawingPoints.indexOf(
+        this._drawingPoints
+      ) > -1
+    ) {
+      Main.getInstance().strategyModel.drawingPoints.splice(
+        Main.getInstance().strategyModel.drawingPoints.indexOf(
+          this._drawingPoints
+        ),
+        1
+      );
+    }
+    Main.getInstance().strategyModel.drawingPoints.push(this._drawingPoints);
+  }
+  undoCommand(): void {
+    if (
+      Main.getInstance().strategyModel.drawingPoints.indexOf(
+        this._drawingPoints
+      ) > -1
+    ) {
+      Main.getInstance().strategyModel.drawingPoints.splice(
+        Main.getInstance().strategyModel.drawingPoints.indexOf(
+          this._drawingPoints
+        ),
+        1
+      );
+    }
+  }
+}
--- frontend/src/app/state/drawing/literals/PenState.ts
+++ frontend/src/app/state/drawing/literals/PenState.ts
@@ -3,6 +3,7 @@ import Pencil from "../../../../assets/img/elements/pencil.svg";
 import Main from "../../../Main";
 import DrawingPoint from "../../../models/DrawingPoint";
 import CoordinateUtils from "../../../utils/CoordinateUtils";
+import DrawLineCommand from "../../../commands/literals/DrawLineCommand";
 
 export default class PenState extends AbstractDrawingState {
   private isDrawing: boolean;
@@ -16,21 +17,34 @@ export default class PenState extends AbstractDrawingState {
   public onMouseDown(event: MouseEvent): void {
     this.isDrawing = true;
     this.temp = [];
-
   }
   public onMouseMove(event: MouseEvent): void {
     if (!this.isDrawing) return;
     const canvas = Main.getInstance().canvas.canvasElement!;
     const mouseX: number = event.clientX - canvas.offsetLeft;
     const mouseY: number = event.clientY - canvas.offsetTop;
-    const worldCoords = CoordinateUtils.screenToWorld({ x: mouseX, y: mouseY }, Main.getInstance().canvas.cameraOffset, Main.getInstance().canvas.cameraZoom);
-    if (Main.getInstance().strategyModel.drawingPoints.indexOf(this.temp) !== -1) {
-      Main.getInstance().strategyModel.drawingPoints.splice(Main.getInstance().strategyModel.drawingPoints.indexOf(this.temp), 1);
+    const worldCoords = CoordinateUtils.screenToWorld(
+      { x: mouseX, y: mouseY },
+      Main.getInstance().canvas.cameraOffset,
+      Main.getInstance().canvas.cameraZoom
+    );
+    if (
+      Main.getInstance().strategyModel.drawingPoints.indexOf(this.temp) !== -1
+    ) {
+      Main.getInstance().strategyModel.drawingPoints.splice(
+        Main.getInstance().strategyModel.drawingPoints.indexOf(this.temp),
+        1
+      );
     }
-    this.temp.push(new DrawingPoint(worldCoords.x, worldCoords.y, this.thickness, this.color));
+    this.temp.push(
+      new DrawingPoint(worldCoords.x, worldCoords.y, this.thickness, this.color)
+    );
     Main.getInstance().strategyModel.drawingPoints.push(this.temp);
   }
   public onMouseUp(): void {
     this.isDrawing = false;
+    Main.getInstance().commandManager.addCommand(
+      new DrawLineCommand(this.temp)
+    );
   }
 }
